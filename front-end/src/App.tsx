a7a4ac2ee7a2b45b8412b42180d4ac56ad62e26b
Andrew Gazhala <andreygazhala@gmail.com>
2026-08-11 18:22:52 +0300

    Front-end app. Added React app foundation

---
+++ front-end/src/App.tsx
@@ -0,0 +1,14 @@
+import "./App.css";
+
+function HelloWorld() {
+  return <h1>Hello, World!</h1>;
+}
+
+export default function App() {
+  return (
+    <div className="App">
+      <HelloWorld />
+      <p>Welcome to my app</p>
+    </div>
+  );
+}
