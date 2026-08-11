c36889f7cee79e7e24afed672dbacd94f6685eef
Andrew Gazhala <andreygazhala@gmail.com>
2026-08-11 15:56:04 +0300

    Update node dependencies with prettier. Added prettier config, update eslint config with preffier.

--- back-end/eslint.config.mjs
+++ back-end/eslint.config.mjs
@@ -1,10 +1,12 @@
 import eslint from "@eslint/js";
+import eslintConfigPrettier from "eslint-config-prettier";
 import globals from "globals";
 import tseslint from "typescript-eslint";
 
 export default tseslint.config(
   eslint.configs.recommended,
   ...tseslint.configs.recommended,
+  eslintConfigPrettier,
   {
     languageOptions: {
       ecmaVersion: 2022,
